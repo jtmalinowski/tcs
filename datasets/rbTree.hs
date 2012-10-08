08f8d28b5a1e0d56cfc5028653c3eac756de2dd7
Jakub Malinowski <jakubmal@gmail.com>
2012-10-08 09:50:27 +0200

    Finished zipper for a binary tree

--- datasets/rbTree.hs
+++ datasets/rbTree.hs
@@ -1,14 +1,29 @@
-data RBTree x = 
-    Node x (RBTree x) (RBTree x)
+data Tree x = 
+    Node x (Tree x) (Tree x)
   | Empty
   deriving(Show)
+leaf x = Node x Empty Empty
 
-tree x = Node x Empty Empty
 
-append value Empty = tree value
-append value tree@(Node x left right)
-  | value < x = Node x (append value left) right
-  | value > x = Node x left (append value right)
-  | value == x = tree
+data Direction = Lft | Rght deriving(Eq,Show)
+--TreeZipper 1. other not chosen tree 2. direction of the chosen tree
+data TreeZipper a = TreeZipper [(a, Tree a, Direction)] deriving(Show)
+newZipper = TreeZipper []
+
+data TreeFocus a = TreeFocus (Tree a) (TreeZipper a) deriving(Show)
+newFocus tree = TreeFocus tree newZipper
+goLeft  (TreeFocus (Node x left right) (TreeZipper crumbs)) = TreeFocus left  (TreeZipper $ (x, right, Lft):crumbs)
+goRight (TreeFocus (Node x left right) (TreeZipper crumbs)) = TreeFocus right (TreeZipper $ (x, left, Rght):crumbs)
+
+goUp (TreeFocus currentBranch (TreeZipper ((value, otherBranch, direction):crumbs) ))
+  | direction == Lft =  TreeFocus (Node value currentBranch otherBranch) (TreeZipper crumbs)
+  | direction == Rght = TreeFocus (Node value otherBranch currentBranch) (TreeZipper crumbs)
+
+sampleTree1 = (Node 2 (leaf 1) (leaf 3))
+sampleTree2 = (Node 6 (leaf 5) (leaf 7))
+sampleContext1 = newFocus sampleTree1
+
+sampleTree3 = (Node 4 sampleTree1 sampleTree2)
+sampleContext3 = newFocus sampleTree3
 
 x -: f = f x
